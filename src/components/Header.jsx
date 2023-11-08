8c77367798e5d3a2b17e2febba8216903d4405fa
yuliyakasavec <dkosovec@reaktivate.com>
2023-11-08 15:27:02 +0100

    Components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+    <header className='header'>
+        <img src='https://img.freepik.com/premium-vector/cat-lover-logo-template-design_169878-217.jpg' />
+      </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+const Profile = () => {
+    return (
+      <div className='content'>
+      <div>
+      <img src='https://shop-cdn1-2.vigbo.tech/shops/2544/products/16486697/images/3-9f0ed407d8ba2537f38b8b2d9765ccdb.jpg' />
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My posts
+        <div>
+          New post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default Profile;

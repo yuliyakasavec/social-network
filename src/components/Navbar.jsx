54f0ba6a748a997d6eef2d4e066c3ea2a2edfb24
yuliyakasavec <dkosovec@reaktivate.com>
2023-11-09 14:33:08 +0100

    css modules

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,21 @@
+import classes from './Navbar.module.css';
+
 const Navbar = () => {
     return (
-      <nav className='nav'>
-      <div>
+      <nav className={classes.nav}>
+      <div className={classes.item}>
         <a>Profile</a>
       </div>
-      <div>
+      <div className={`${classes.item} ${classes.active}`}>
         <a>Messages</a>
       </div>
-      <div>
+      <div className={classes.item}>
         <a>News</a>
       </div>
-      <div>
+      <div className={classes.item}>
         <a>Music</a>
       </div>
-      <div>
+      <div className={classes.item}>
         <a>Settings</a>
       </div>
     </nav>

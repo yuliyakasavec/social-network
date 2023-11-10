dba52e9f27c1256b07393f52e44b1c19949e7aa5
yuliyakasavec <dkosovec@reaktivate.com>
2023-11-10 16:39:26 +0100

    added NavLink to users in Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import classes from "./Dialogs.module.css";
 
 const Dialogs = () => {
@@ -5,22 +6,22 @@ const Dialogs = () => {
     <div className={classes.dialogs}>
       <div className={classes.dialogsItems}>
         <div className={`${classes.dialog} ${classes.active}`}>
-        Yuliya
+        <NavLink to="/dialogs/1">Yuliya</NavLink>
         </div>
         <div className={classes.dialog}>
-        Denis
+        <NavLink to="/dialogs/2">Denis</NavLink>
         </div>
         <div className={classes.dialog}>
-        Vika
+        <NavLink to="/dialogs/3">Vika</NavLink>
         </div>
         <div className={classes.dialog}>
-        Pavel
+        <NavLink to="/dialogs/4">Pavel</NavLink>
         </div>
         <div className={classes.dialog}>
-        Katya
+        <NavLink to="/dialogs/5">Katya</NavLink>
         </div>
         <div className={classes.dialog}>
-        Vita
+        <NavLink to="/dialogs/6">Vita</NavLink>
         </div>
         </div>
         <div className={classes.messages}>

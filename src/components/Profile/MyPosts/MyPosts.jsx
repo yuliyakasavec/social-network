90042dd1531bc592583cebb4eef69b7a644659f0
yuliyakasavec <dkosovec@reaktivate.com>
2023-11-10 18:23:54 +0100

    UI-BLL

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,6 +6,16 @@ import Post from './Post/Post';
 //   likeCount: '30'
 // }
 const MyPosts = (props) => {
+
+  let postData = [
+    {id: 1, message: 'Hi, how are you?', likeCount: '30'}, 
+    {id: 2, message: 'It is my first post', likeCount: '35'},
+    {id: 3, message: 'Bye', likeCount: '40'},
+    {id: 4, message: 'Fine'},
+    {id: 5, message: 'Nice to meet you'},
+    {id: 6, message: 'Ok'}
+  ]
+
     return (
       <div className={classes.postsBlock}>
         <h3>My posts</h3>
@@ -18,8 +28,9 @@ const MyPosts = (props) => {
           </div>
         </div>
         <div className={classes.posts}>
-          <Post message='Hi, how are you?' likeCount='30' />
-          <Post message='It is my first post' likeCount='35' />
+          <Post message={postData[0].message} id={postData[0].id} likeCount={postData[0].likeCount} />
+          <Post message={postData[1].message} id={postData[1].id} likeCount={postData[1].likeCount} />
+          <Post message={postData[2].message} id={postData[2].id} likeCount={postData[2].likeCount} />
         </div>
       </div>
     )

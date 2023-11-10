d5ee32f7e9dd86f3cb5b1a5f7b1d137afd66edea
yuliyakasavec <dkosovec@reaktivate.com>
2023-11-10 17:22:24 +0100

    added props to Dialogs, created ProfileInfo

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,11 +7,15 @@ import Post from './Post/Post';
 // }
 const MyPosts = (props) => {
     return (
-      <div>
-        My posts
+      <div className={classes.postsBlock}>
+        <h3>My posts</h3>
         <div>
+          <div>
           <textarea></textarea>
+          </div>
+          <div>
           <button>Add post</button>
+          </div>
         </div>
         <div className={classes.posts}>
           <Post message='Hi, how are you?' likeCount='30' />
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,18 +1,14 @@
-import MyPosts from './MyPosts/MyPosts';
-import classes from './Profile.module.css';
+import MyPosts from "./MyPosts/MyPosts";
+import classes from "./Profile.module.css";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
-    return (
-      <div>
-      <div>
-      <img className={classes.contentImage} src='https://shop-cdn1-2.vigbo.tech/shops/2544/products/16486697/images/3-9f0ed407d8ba2537f38b8b2d9765ccdb.jpg' />
-      </div>
-      <div>
-        ava + description
-      </div>
+  return (
+    <div>
+      <ProfileInfo />
       <MyPosts />
     </div>
-    )
-}
+  );
+};
 
 export default Profile;

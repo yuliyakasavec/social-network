6878e9bbef01da905f4614bcffe29faf3a85320b
yuliyakasavec <dkosovec@reaktivate.com>
2023-11-10 16:17:30 +0100

    added names of users in Dialogs and added messages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,35 @@
-import classes from './Dialogs.module.css';
+import classes from "./Dialogs.module.css";
 
 const Dialogs = () => {
-    return (
-      <div>
-        Dialogs
-      </div>
-    )
-}
+  return (
+    <div className={classes.dialogs}>
+      <div className={classes.dialogsItems}>
+        <div className={`${classes.dialog} ${classes.active}`}>
+        Yuliya
+        </div>
+        <div className={classes.dialog}>
+        Denis
+        </div>
+        <div className={classes.dialog}>
+        Vika
+        </div>
+        <div className={classes.dialog}>
+        Pavel
+        </div>
+        <div className={classes.dialog}>
+        Katya
+        </div>
+        <div className={classes.dialog}>
+        Vita
+        </div>
+        </div>
+        <div className={classes.messages}>
+          <div className={classes.message}>Hi</div>
+          <div className={classes.message}>How are you?</div>
+          <div className={classes.message}>Bye</div>
+        </div>
+    </div>
+  );
+};
 
 export default Dialogs;

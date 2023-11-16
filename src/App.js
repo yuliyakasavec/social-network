ee41babdc0dc5fa6b70bf193959feede65c1d5df
yuliyakasavec <dkosovec@reaktivate.com>
2023-11-16 18:06:14 +0100

    added onClick, ref

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ import Profile from "./components/Profile/Profile";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import Friends from "./components/Friends/Friends";
+
 
 const App = (props) => {
-
   return (
     
       <div className="app-wrapper">
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import state from './redux/state';
 import { BrowserRouter } from 'react-router-dom';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

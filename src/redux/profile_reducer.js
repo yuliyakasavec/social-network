2f2e754d99497bf33dae36a963a37b5123de8eb7
yuliyakasavec <dkosovec@reaktivate.com>
2024-04-19 11:06:15 +0200

    added redux

--- src/redux/profile_reducer.js
+++ src/redux/profile_reducer.js
@@ -1,7 +1,19 @@
 const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 
-const profileReducer = (state, action) => {
+let initialState = {
+    posts: [
+      { id: 1, message: "Hi, how are you?", likesCount: "30" },
+      { id: 2, message: "It is my first post", likesCount: "35" },
+      { id: 3, message: "Bye", likesCount: "40" },
+      { id: 4, message: "Fine", likesCount: "10" },
+      { id: 5, message: "Nice to meet you", likesCount: "15" },
+      { id: 6, message: "Ok", likesCount: "50" },
+    ],
+    newPostText: "it-kamasutra.com",
+  };
+
+const profileReducer = (state = initialState, action) => {
     switch(action.type) {
         case ADD_POST:
             let newPost = {

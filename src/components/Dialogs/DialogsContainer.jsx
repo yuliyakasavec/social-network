b44805795519ef3d6a7d88a618edd7164f90e7e5
yuliyakasavec <dkosovec@reaktivate.com>
2024-04-24 18:07:35 +0200

    added Context

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,20 +1,33 @@
 import React from "react";
-import { sendMessageActionCreator, updateNewMessageTextActionCreator } from "../../redux/dialogs_reducer";
+import {
+  sendMessageActionCreator,
+  updateNewMessageTextActionCreator,
+} from "../../redux/dialogs_reducer";
 import Dialogs from "./Dialogs";
+import StoreContext from "../../StoreContext";
 
-const DialogsContainer = (props) => {
+const DialogsContainer = () => {
+  return (
+    <StoreContext.Consumer> 
+      {(store) => {
+        let state = store.getState().dialogsPage;
 
-  let state = props.store.getState().dialogsPage;
+        let sendMessage = () => {
+          store.dispatch(sendMessageActionCreator());
+        };
 
-  let sendMessage = () => {
-    props.store.dispatch(sendMessageActionCreator());
-  };
-
-  let onMessageChange = (text) => {
-    let action = updateNewMessageTextActionCreator(text);
-    props.store.dispatch(action);
-  }
-  return (<Dialogs updateNewMessageText={onMessageChange} sendMessage={sendMessage} dialogsPage={state} />)
+        let onMessageChange = (text) => {
+          let action = updateNewMessageTextActionCreator(text);
+          store.dispatch(action);
+        };
+        return <Dialogs
+            updateNewMessageText={onMessageChange}
+            sendMessage={sendMessage}
+            dialogsPage={state}
+          />}
+          }
+    </StoreContext.Consumer>
+  );
 };
 
 export default DialogsContainer;

aec2f2ee33983d19ccbb0ded16ae7b37e8a14188
yuliyakasavec <kosoveculia8@gmail.com>
2024-05-08 18:59:30 +0200

    side effect

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,6 +4,8 @@ import axios from 'axios';
 import userPhoto from '../../assets/images/user.png'
 
 let Users = (props) => {
+
+  let getUsers = () => {
   if (props.users.length === 0) {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
@@ -11,9 +13,11 @@ let Users = (props) => {
         props.setUsers(response.data.items);
       });
   }
+}
 
   return (
     <div>
+      <button onClick={getUsers}>Get Users</button>
       {props.users.map((u) => (
         <div key={u.id}>
           <span>

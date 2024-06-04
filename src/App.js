8de36652e6679fc070b0668b83989f77e04d9055
yuliyakasavec <kosoveculia8@gmail.com>
2024-06-04 22:45:46 +0300

    Navigate to/Redirect

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
+import Login from "./components/Login/Login";
 
 
 const App = (props) => {
@@ -20,6 +21,7 @@ const App = (props) => {
             <Route path="/dialogs/*" element={<DialogsContainer />} />
             <Route path="/profile/:userId?" element={<ProfileContainer />} />
             <Route path="/users/*" element={<UsersContainer />} />
+            <Route path="/login/*" element={<Login />} />
             <Route path="/news" element={<News />} />
             <Route path="/music" element={<Music />} />
             <Route path="/settings" element={<Settings />} />
